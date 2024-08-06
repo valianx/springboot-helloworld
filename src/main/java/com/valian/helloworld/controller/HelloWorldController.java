a4a786f11e25de095c428e1b2bb242d331b63ba4
Mario Gutierrez <mgutierrez@stratechcorp.com>
2024-08-06 01:51:39 -0400

    update dockerfile

--- src/main/java/com/valian/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/valian/helloworld/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloWorldController {
 
-    @GetMapping("/hello")
+    @GetMapping("/world")
     public String helloWorld() {
         return "Hello World";
     }
